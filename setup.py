09464854da9e6368c6d645f4f0dbe7066cb9ab95
Titas <titas.januson@gmail.com>
2021-01-13 08:47:11 +0200

    Update name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="SyroQT-Calculator",
+    name="SyroQTCalculator",
     version="0.0.1",
     author="Titas Janusonis",
     description="A simple calculator",
